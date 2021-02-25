5ad9ebf41a59f494fe019ac023345eb47d04e217
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2021-02-25 14:10:57 -0300

    :fire: feat: CategoryRepository

---
+++ src/main/java/com/ghostdovahkiin/librapi/bookservice/category/CategoryRepository.java
@@ -0,0 +1,6 @@
+package com.ghostdovahkiin.librapi.bookservice.category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
